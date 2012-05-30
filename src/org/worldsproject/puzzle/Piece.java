96ed30f7e9b3cbb87c786d4542cd7e048976b9b2
Tim Butram <atrus6@gmail.com>
2012-05-30 14:40:28 -0400

    Initial Puzzle Piece.

---
+++ src/org/worldsproject/puzzle/Piece.java
@@ -0,0 +1,57 @@
+package org.worldsproject.puzzle;
+
+import android.graphics.Bitmap;
+
+public class Piece
+{
+	public enum Type
+	{
+		CORNER, EDGE, FULL; 
+	}
+	
+	private int x = 0;
+	private int y = 0;
+	
+	/*
+	 * 0 is the correct orientation.
+	 * 1 is 90 degrees turned clockwise.
+	 * 2 is 180 degrees turned clockwise.
+	 * 3 is 270 degrees turned clockwise.
+	 */
+	private int orientation = 0;
+	
+	public Piece(Bitmap image)
+	{
+		
+	}
+	
+	public void turn()
+	{
+		orientation++;
+		
+		if(orientation >= 4)
+		{
+			orientation = 0;
+		}
+	}
+
+	public int getX()
+	{
+		return x;
+	}
+
+	public void setX(int x)
+	{
+		this.x = x;
+	}
+
+	public int getY()
+	{
+		return y;
+	}
+
+	public void setY(int y)
+	{
+		this.y = y;
+	}
+}
