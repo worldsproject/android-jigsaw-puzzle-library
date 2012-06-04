ad06ebd5b06996aef6114118017a461032afc2ef
Tim Butram <atrus6@gmail.com>
2012-06-04 12:32:56 -0400

    Added a group class for grouped pieces.

---
+++ src/org/worldsproject/puzzle/PuzzleGroup.java
@@ -0,0 +1,32 @@
+package org.worldsproject.puzzle;
+
+import java.util.ArrayList;
+
+public class PuzzleGroup
+{
+	private ArrayList<Piece> group = new ArrayList<Piece>();
+	
+	public PuzzleGroup(){}
+	
+	public void addPiece(Piece piece)
+	{
+		group.add(piece);
+	}
+	
+	public void translate(int x, int y)
+	{
+		for(Piece p : group)
+		{
+			p.setX(p.getX()+x);
+			p.setY(p.getY()+y);
+		}
+	}
+	
+	public void addGroup(PuzzleGroup group)
+	{
+		for(Piece p : group.group)
+		{
+			this.group.add(p);
+		}
+	}
+}
