3c3bffe05c0d9fa3ef4cc93f582a5267c8c9ed50
Tim Butram <atrus6@gmail.com>
2012-09-25 13:50:41 -0400

    Removing Renderscript items.

--- src/org/worldsproject/puzzle/PuzzleSelectActivity_Post3.java
+++ src/org/worldsproject/puzzle/PuzzleSelectActivity_Post3.java
@@ -1,25 +1,14 @@
 package org.worldsproject.puzzle;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.Gallery;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 public class PuzzleSelectActivity_Post3 extends Activity {
+    
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		this.setContentView(R.layout.selector_post3);
 	}
 }
