3abfcfbfe30113f2e4c0f0f9e8fb215021e80be2
Tim Butram <atrus6@gmail.com>
2012-09-26 19:14:06 -0400

    Properly setup the coverflow.

--- src/org/worldsproject/puzzle/PuzzleSelectActivity.java
+++ src/org/worldsproject/puzzle/PuzzleSelectActivity.java
@@ -1,29 +1,24 @@
 package org.worldsproject.puzzle;
 
+import org.worldsproject.puzzle.coverflow.CoverFlow;
+import org.worldsproject.puzzle.coverflow.ReflectingImageAdapter;
+import org.worldsproject.puzzle.coverflow.ResourceImageAdapter;
+
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.BaseAdapter;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
-import android.widget.Gallery;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 public class PuzzleSelectActivity extends Activity
 {
-	private ImageView selected;
 	private Button button;
 	
-	private int image;
 	private String difficulty = "easy";
+	private int[] images;
 	private Intent intent;
 	
 	@Override
@@ -33,51 +28,25 @@ public class PuzzleSelectActivity extends Activity
 		
 		setContentView(R.layout.selector);
 		
-		Animation fadeIn = new AlphaAnimation(0, 1);
-		fadeIn.setDuration(1500);
-		
-		fadeIn.setRepeatCount(Animation.INFINITE);
-		fadeIn.setRepeatMode(Animation.REVERSE);
-		
-		TextView ins = (TextView) findViewById(R.id.instruct);
-		ins.startAnimation(fadeIn);
-		Gallery gallery = (Gallery) findViewById(R.id.gallery);
-		gallery.setAdapter(new ImageAdapter(this, this.getIntent().getIntArrayExtra("images")));
-		gallery.setSpacing(1);
-		
-		selected = (ImageView) findViewById(R.id.imageView);
-
-		gallery.setOnItemSelectedListener(new OnItemSelectedListener()
-		{
-
-			@Override
-			public void onItemSelected(AdapterView<?> arg0, View arg1,
-					int arg2, long arg3)
-			{
-				ImageAdapter ia = (ImageAdapter)arg0.getAdapter();
-				image = (int)ia.getItemId(arg2);
-				selected.setImageDrawable(getResources().getDrawable(image));
-			}
-
-			@Override
-			public void onNothingSelected(AdapterView<?> arg0)
-			{
-			}
-		});
+		CoverFlow cf = (CoverFlow)findViewById(R.id.coverflow);
+		ResourceImageAdapter ria = new ResourceImageAdapter(this);
+		images = this.getIntent().getIntArrayExtra("images");
+		ria.setResources(images);
+		ReflectingImageAdapter coverImageAdapter = new ReflectingImageAdapter(ria);
+		cf.setAdapter(coverImageAdapter);
+        cf.setSelection(0, true);
 		
 		intent = new Intent(this, PuzzleSolveActivity.class);
 		
-		selected.setOnClickListener(new OnClickListener()
-		{
-			@Override
-			public void onClick(View v)
-			{
-				intent.putExtra("image", image);
-				intent.putExtra("difficulty", difficulty);
+		cf.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(final AdapterView< ? > parent, final View view, final int position, final long id) {
+                intent.putExtra("image", images[position]);
+                intent.putExtra("difficulty", difficulty);
 				PuzzleSelectActivity.this.startActivity(intent);
-			}
-			
-		}); 
+            }
+
+        });
 		
 		button = (Button)findViewById(R.id.difficulty);
 		
@@ -104,45 +73,5 @@ public class PuzzleSelectActivity extends Activity
 			}
 			
 		});
-	}
-
-	private class ImageAdapter extends BaseAdapter
-	{
-		private Context mContext;
-
-		private int[] mImageIds;
-
-		public ImageAdapter(Context c, int[] ids)
-		{
-			mContext = c;
-			mImageIds = ids;
-		}
-
-		public int getCount()
-		{
-			return mImageIds.length;
-		}
-
-		public Object getItem(int position)
-		{
-			return mImageIds[position];
-		}
-
-		public long getItemId(int position)
-		{
-			return mImageIds[position];
-		}
-
-		public View getView(int position, View convertView, ViewGroup parent)
-		{
-			ImageView imageView = new ImageView(mContext);
-
-			imageView.setImageResource(mImageIds[position]);
-			imageView.setLayoutParams(new Gallery.LayoutParams(150, 100));
-			imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
-
-			return imageView;
-		}
-
 	}
 }
